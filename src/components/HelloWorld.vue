15c7c2b4d66edd7353561d20c0881a69e640e27d
Waaatanuki <waaatanuki@gmail.com>
2022-02-27 13:51:25 +0800

    deploy

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h3>开发中!!!</h3>
+    <h3>开发中...</h3>
     <h3>{{ hi }}</h3>
   </div>
 </template>
@@ -9,16 +9,17 @@
 <script>
 export default {
   name: "HelloWorld",
-  props: {
-    msg: String,
-  },
   setup() {
     let hi = "(<ゝω・)☆";
+
+    return { hi };
+  },
+  props: {
+    msg: String,
   },
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
   margin: 40px 0 0;
