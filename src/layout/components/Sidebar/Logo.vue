385fb8698b4979b85c5629fa445ff387dfeaa76e
Waaatanuki <watanukipublic@gmail.com>
2023-02-06 18:52:02 +0800

    fix: 修复侧边栏标题不显示问题

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -1,4 +1,40 @@
+<script lang="ts" setup>
+defineProps({
+  collapse: {
+    type: Boolean,
+    required: true,
+  },
+})
+const sidebarLogo = ref(true)
+const logo = ref<string>(new URL(`/images/logo.png`, import.meta.url).href)
+const title = ref('古兰布鲁')
+</script>
+
 <template>
+  <transition>
+    <router-link
+      v-if="collapse"
+      key="collapse"
+      class="h-[50px] flex items-center justify-center"
+      to="/"
+    >
+      <img v-if="sidebarLogo" :src="logo" class="w-8 h-8" />
+      <h1 v-else>{{ title }}</h1>
+    </router-link>
+
+    <router-link
+      v-else
+      key="expand"
+      class="h-[50px] flex items-center justify-center"
+      to="/"
+    >
+      <img v-if="sidebarLogo" :src="logo" class="w-8 h-8" />
+      <span class="ml-3 text-black text-sm font-bold">{{ title }}</span>
+    </router-link>
+  </transition>
+</template>
+
+<!-- <template>
   <div class="sidebar-logo-container" :class="{ collapse: isCollapse }">
     <transition name="sidebarLogoFade">
       <router-link
@@ -16,9 +52,9 @@
       </router-link>
     </transition>
   </div>
-</template>
+</template> -->
 
-<script setup lang="ts">
+<!-- <script setup lang="ts">
 import { ref, reactive, toRefs } from 'vue'
 
 const props = defineProps({
@@ -40,51 +76,4 @@ const title = ref('古兰布鲁')
 </script>
 
 <style lang="scss" scoped>
-.sidebarLogoFade-enter-active {
-  transition: opacity 1.5s;
-}
-
-.sidebarLogoFade-enter,
-.sidebarLogoFade-leave-to {
-  opacity: 0;
-}
-
-.sidebar-logo-container {
-  position: relative;
-  width: 100%;
-  height: 50px;
-  line-height: 50px;
-  text-align: center;
-  overflow: hidden;
-  border-bottom: #3375b9 3px solid;
-  & .sidebar-logo-link {
-    height: 100%;
-    width: 100%;
-
-    & .sidebar-logo {
-      width: 32px;
-      height: 32px;
-      vertical-align: middle;
-      background: #fff;
-    }
-
-    & .sidebar-title {
-      display: inline-block;
-      margin: 0;
-      font-weight: 600;
-      line-height: 50px;
-      font-size: 14px;
-      font-family: Avenir, Helvetica Neue, Arial, Helvetica, sans-serif;
-      vertical-align: middle;
-      margin-left: 12px;
-      color: #3375b9;
-    }
-  }
-
-  &.collapse {
-    .sidebar-logo {
-      margin-right: 0px;
-    }
-  }
-}
-</style>
+</style> -->
