b276b954c48ed5995024399823c1058e7db594e0
Waaatanuki <watanukipublic@gmail.com>
2023-11-01 15:23:41 +0800

    chore: 更新2023.11月日程表

--- src/components/Schedule.vue
+++ src/components/Schedule.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import dayjs from 'dayjs'
-import { schedule } from '~/constants'
+import scheduleData from '~/assets/data/schedule.json'
 
 const handleColor = function (start: any, end: any) {
   if (dayjs().isBefore(dayjs(start)) && dayjs(start).diff(dayjs(), 'h') < 24)
@@ -23,9 +23,9 @@ const { height } = useWindowSize()
       </div>
     </template>
     <el-scrollbar :max-height="height - 330">
-      <el-timeline v-if="dayjs().isSame(dayjs(schedule[0].end), 'month')" style="padding: 0 15px 0 0">
+      <el-timeline v-if="dayjs().isSame(dayjs(scheduleData[0].end), 'month')" style="padding: 0 15px 0 0">
         <el-timeline-item
-          v-for="activity, idx in schedule" :key="idx" placement="top"
+          v-for="activity, idx in scheduleData" :key="idx" placement="top"
           :timestamp="`${dayjs(activity.start).format('MM/DD HH:mm')} - ${dayjs(activity.end).format('MM/DD HH:mm')}`"
           :color="handleColor(activity.start, activity.end)"
         >
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,45 +1,6 @@
 import type { AppGoldBrickTableData } from 'goldBrick'
 import { Raid_GoldBrick } from './raid'
 
-export const schedule = [
-  {
-    title: '月末战货',
-    comment: '',
-    start: '2023-09-28 19:00',
-    end: '2023-10-06 20:59',
-  },
-  {
-    title: '转世外传',
-    comment: '',
-    start: '2023-10-07 17:00',
-    end: '2023-10-13 16:59',
-  },
-  {
-    title: '公会战',
-    comment: '',
-    start: '2023-10-14 19:00',
-    end: '2023-10-20 18:59',
-  },
-  {
-    title: '剧情复刻',
-    comment: '',
-    start: '2023-10-21 17:00',
-    end: '2023-10-28 18:59',
-  },
-  {
-    title: '巴布塔',
-    comment: '',
-    start: '2023-10-24 19:00',
-    end: '2023-10-31 18:59',
-  },
-  {
-    title: '月末战货',
-    comment: '',
-    start: '2023-10-29 17:00',
-    end: '2023-11-06 20:59',
-  },
-]
-
 export const defaultTodoList = [
   { done: false, content: '巡岛' },
   { done: false, content: '大巴' },
